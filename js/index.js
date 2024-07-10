39d74927804801124984997e6cfccc9ab80f30bb
cychenhaibin <3242477764@qq.com>
2024-07-11 01:18:46 +0800

    第三次提交：添加图标，首页完成

--- js/index.js
+++ js/index.js
@@ -31,7 +31,30 @@ function initSwitch(switchId,onOrOffValue){
 
 //初始化第一个switch开关
 initSwitch("switch-body1",switchForm.switchValue1)
-
+//初始化第二个switch开关
+initSwitch("switch-body2",switchForm.switchValue2)
+//初始化第三个switch开关
+initSwitch("switch-body3",switchForm.switchValue3)
+//初始化第四个switch开关
+initSwitch("switch-body4",switchForm.switchValue4)
+//初始化第五个switch开关
+initSwitch("switch-body5",switchForm.switchValue5)
+//初始化第六个switch开关
+initSwitch("switch-body6",switchForm.switchValue6)
+//初始化第七个switch开关
+initSwitch("switch-body7",switchForm.switchValue7)
+//初始化第八个switch开关
+initSwitch("switch-body8",switchForm.switchValue8)
+//初始化第九个switch开关
+initSwitch("switch-body9",switchForm.switchValue9)
+//初始化第十个switch开关
+initSwitch("switch-body10",switchForm.switchValue10)
+//初始化第十一个switch开关
+initSwitch("switch-body11",switchForm.switchValue11)
+//初始化第十二个switch开关
+initSwitch("switch-body12",switchForm.switchValue12)
+//初始化第十三个switch开关
+initSwitch("switch-body13",switchForm.switchValue13)
 
 //第一个switch开关绑定鼠标点击事件
 document.getElementById("switch-body1").onclick=function(e){
@@ -40,6 +63,83 @@ document.getElementById("switch-body1").onclick=function(e){
     console.log(switchForm.switchValue1)
 }
 
+//第二个switch开关绑定鼠标点击事件
+document.getElementById("switch-body2").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue2 = document.getElementById('switch-body2').getAttribute('value')
+    console.log(switchForm.switchValue2)
+}
+
+//第三个switch开关绑定鼠标点击事件
+document.getElementById("switch-body3").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue3 = document.getElementById('switch-body3').getAttribute('value')
+    console.log(switchForm.switchValue3)
+}
+
+//第四个switch开关绑定鼠标点击事件
+document.getElementById("switch-body4").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue4 = document.getElementById('switch-body4').getAttribute('value')
+    console.log(switchForm.switchValue4)
+}
+
+//第五个switch开关绑定鼠标点击事件
+document.getElementById("switch-body5").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue5 = document.getElementById('switch-body5').getAttribute('value')
+    console.log(switchForm.switchValue5)
+}
+
+//第六个switch开关绑定鼠标点击事件
+document.getElementById("switch-body6").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue6 = document.getElementById('switch-body6').getAttribute('value')
+    console.log(switchForm.switchValue6)
+}
+
+//第七个switch开关绑定鼠标点击事件
+document.getElementById("switch-body7").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue7 = document.getElementById('switch-body7').getAttribute('value')
+    console.log(switchForm.switchValue7)
+}
+//第八个switch开关绑定鼠标点击事件
+document.getElementById("switch-body8").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue8 = document.getElementById('switch-body8').getAttribute('value')
+    console.log(switchForm.switchValue8)
+}
+//第九个switch开关绑定鼠标点击事件
+document.getElementById("switch-body9").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue9 = document.getElementById('switch-body9').getAttribute('value')
+    console.log(switchForm.switchValue9)
+}
+//第十个switch开关绑定鼠标点击事件
+document.getElementById("switch-body10").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue10 = document.getElementById('switch-body10').getAttribute('value')
+    console.log(switchForm.switchValue10)
+}
+//第十一个switch开关绑定鼠标点击事件
+document.getElementById("switch-body11").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue11 = document.getElementById('switch-body11').getAttribute('value')
+    console.log(switchForm.switchValue11)
+}
+//第十二个switch开关绑定鼠标点击事件
+document.getElementById("switch-body12").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue12 = document.getElementById('switch-body12').getAttribute('value')
+    console.log(switchForm.switchValue12)
+}
+//第十三个switch开关绑定鼠标点击事件
+document.getElementById("switch-body13").onclick=function(e){
+    switchClick(e.currentTarget.children[2])
+    switchForm.switchValue13 = document.getElementById('switch-body13').getAttribute('value')
+    console.log(switchForm.switchValue13)
+}
 
 function switchClick(e){
     var switchOn = e.parentNode.children[1]
